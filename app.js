8367df2c855cf5b74f2714e07827eb059e2454ce
Johannus V <johannus.vogel@gmail.com>
2016-10-17 21:05:28 -0600

    Node, Express and Rita have shown up for the party.

---
+++ app.js
@@ -0,0 +1,35 @@
+require('./app/index')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Setting handlebar template helper
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+// Routing
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John'
+  })
+})
+
+
+// Error catching
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
